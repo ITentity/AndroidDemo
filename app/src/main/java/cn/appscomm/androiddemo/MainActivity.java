d9f36f2a8bea6583466bb95e655827dcfd45ca30
ITentity <zzx5888@qq.com>
2017-06-09 11:41:33 +0800

    增加按钮

--- app/src/main/java/cn/appscomm/androiddemo/MainActivity.java
+++ app/src/main/java/cn/appscomm/androiddemo/MainActivity.java
@@ -2,12 +2,37 @@ package cn.appscomm.androiddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Toast.makeText(this,"开启音乐服务",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this,"关闭音乐服务",Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
